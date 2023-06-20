511fabeaf33911194f508960f2808a43d29ba089
RohanD99 <rohan.d@technovert.com>
2023-06-20 19:36:06 +0530

    Single Parent Multiple Child components

---
+++ src/app/employees/employees.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmployeeService } from '../employee.service';
+import { EmployeeCardComponent } from '../employee-card/employee-card.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    
+    
+  ]
+  
+})
+export class EmployeesModule { }
